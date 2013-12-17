d227b627f6785e9c99566cd705544c5baf7939d8
Andrew Sweet <andrewmsweet@gmail.com>
2013-12-17 00:28:53 -0800

    Initial Commit

---
+++ Beacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Beacon
+//
+//  Created by Andrew Sweet on 12/17/13.
+//  Copyright (c) 2013 Beacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BCAppDelegate class]));
+    }
+}
