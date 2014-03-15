d1e79d0cfdc80ec42ba6172fbcb0cdb8bbdecdc5
Andrew Sweet <andrewmsweet@gmail.com>
2014-03-15 19:29:04 -0400

    Want to push to Joel

---
+++ Beacon/BCNInviteCell.m
@@ -0,0 +1,29 @@
+//
+//  BCNInviteCell.m
+//  Fizz
+//
+//  Created by Andrew Sweet on 3/15/14.
+//  Copyright (c) 2014 Fizz. All rights reserved.
+//
+
+#import "BCNInviteCell.h"
+
+@implementation BCNInviteCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
