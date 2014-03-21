1500612d57175a4084a5a19034931d86d727e7f8
Andrew Sweet <andrewmsweet@gmail.com>
2014-03-21 17:58:23 -0400

    Chat in proper view position

---
+++ Beacon/BCNChatDelegate.h
@@ -0,0 +1,13 @@
+//
+//  BCNChatDelegate.h
+//  Fizz
+//
+//  Created by Andrew Sweet on 3/21/14.
+//  Copyright (c) 2014 Fizz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCNChatDelegate : NSObject
+
+@end
---
+++ Beacon/BCNChatDelegate.m
@@ -0,0 +1,13 @@
+//
+//  BCNChatDelegate.m
+//  Fizz
+//
+//  Created by Andrew Sweet on 3/21/14.
+//  Copyright (c) 2014 Fizz. All rights reserved.
+//
+
+#import "BCNChatDelegate.h"
+
+@implementation BCNChatDelegate
+
+@end
